f57f301d1cb2a5ed6c249fa48941ee0f3b7a54f3
João victor Belizario dos santos <113644605+BelizarioJv@users.noreply.github.com>
2025-07-16 12:07:24 -0300

    Update carrosGaragem.py

--- carrosGaragem.py
+++ carrosGaragem.py
@@ -31,16 +31,14 @@ while True :
   continuar = input('Deseja cadastrar outro veiculo?(S/N)').strip().upper()
   if continuar != 'S':
     break
-  mediaDiaVeiculos = sum(numeroDias_Matriz)/ len(numeroDias_Matriz)
+  numDias = sum(numeroDias_Matriz) 
+  numDiasLen = len(numeroDias_Matriz)
+  mediaDiaVeiculos = numDias/ numDiasLen
 print('-                                                             -')
 print('Veiculos cadastrados com sucesso!!')
 print('************************************')
-print(f'A soma total de dias dos veiculos e:{(sum(numeroDias_Matriz))}')
-print(f'O modelo que ficara mais tempo na garagem e o:{modeloVeiculo_Matriz[0]} com um toal de {numeroDias_Matriz[0]}')
-print(f'O numero de veiculos na garagem e de : {len(modeloVeiculo_Matriz)}')
-print(f'A media de dias dos veiculos e de:{mediaDiaVeiculos}')
-print('-                                                             -')
-   
+print(f'A soma total de dias dos veiculos e:{numDias}')
+print(f'O modelo que ficara mais tempo na garagem
 
 
 
