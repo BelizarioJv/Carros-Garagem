2e6545a8684f1231e7e2ae55989475b7dcf985eb
João victor Belizario dos santos <113644605+BelizarioJv@users.noreply.github.com>
2025-07-29 17:23:26 -0300

    Update carrosGaragem.py

--- carrosGaragem.py
+++ carrosGaragem.py
@@ -1,45 +1,33 @@
-modeloVeiculo_Matriz = []
-numeroDias_Matriz = []
-totaisDias = 0
-veiculoMaisTempo_nome= ''
-veiculoMaisTempo = 0
-totalVeiculos = 0
-totalDiaVeiculo = 0
-
 while True :
-  modeloVeiculo = input('Digite o modelo do veiculo')
-  TotalDiaVeiculo_str = input('Digite o total de dias que o veiculo vai ficar')
+  totaisDias = 0
+  veiculoMaisTempo_nome= ''
+  veiculoMaisTempo = 0
+  totalVeiculos = 0
+  modeloVeiculo = input('Digite o modelo do veiculo: ')
+  TotalDiaVeiculo_str = input('Digite o total de dias que o veiculo vai ficar: ')
 
   if TotalDiaVeiculo_str.isdigit():
-    totalDiaVeiculo = int(TotalDiaVeiculo_str)
+   totalDiaVeiculo = int(TotalDiaVeiculo_str)
   else:
     print('Digito invalido')
     break
-  
+
+  totaisDias += totalDiaVeiculo
+  totalVeiculos += 1 
 
   if veiculoMaisTempo < totalDiaVeiculo:
     veiculoMaisTempo_nome = modeloVeiculo
     veiculoMaisTempo = totalDiaVeiculo
 
-    modeloVeiculo_Matriz.insert(0,veiculoMaisTempo_nome)
-    numeroDias_Matriz.insert(0,veiculoMaisTempo)
-  else:
-    modeloVeiculo_Matriz.append([modeloVeiculo])
-    numeroDias_Matriz.append([totalDiaVeiculo])
-  
-
   continuar = input('Deseja cadastrar outro veiculo?(S/N)').strip().upper()
   if continuar != 'S':
     break
-  numDias = sum(numeroDias_Matriz) 
-  numDiasLen = len(numeroDias_Matriz)
-  mediaDiaVeiculos = numDias/ numDiasLen
+
+mediaDiaVeiculos = totalDiaVeiculo / totalVeiculos
 print('-                                                             -')
 print('Veiculos cadastrados com sucesso!!')
 print('************************************')
-print(f'A soma total de dias dos veiculos e:{numDias}')
-print(f'O modelo que ficara mais tempo na garagem
-
-
-
-
+print(f'O modelo que ficara mais tempo na garagem é o: {veiculoMaisTempo_nome} que vai ficar {veiculoMaisTempo}')
+print(f'O numero de veiculos na garagem é de : {totalVeiculos}')
+print(f'A media de dias dos veiculos é de: {mediaDiaVeiculos:.0f}')
+print('-                                                             -')
